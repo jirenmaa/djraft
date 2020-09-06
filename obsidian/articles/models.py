27d3407daa93170e483d46f27f2be51bcd47211b
jirenmaa <jirenmaa@gmail.com>
2020-09-06 22:42:40 +0700

    [update] n2

--- obsidian/articles/models.py
+++ obsidian/articles/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+from core.models import TimestampedModel
+from users.models import User
+
+
+class Tag(TimestampedModel):
+    slug = models.SlugField(unique=True)
+    tag = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.tag
+
+
+class Article(TimestampedModel):
+    slug = models.SlugField(max_length=100, unique=True)
+    title = models.CharField(max_length=100)
+
+    # Every article must have an author. This will answer questions like "Who
+    # gets credit for writing this article?" and "Who can edit this article?".
+    # Unlike the `User` <-> `Profile` relationship, this is a simple foreign
+    # key (or one-to-many) relationship. In this case, one `Profile` can have
+    # many `Article`s.
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="articles")
+
+    description = models.TextField()
+    body = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name="articles")
+
+    def __str__(self):
+        return self.title
+
+
+class Comments(TimestampedModel):
+    body = models.TextField()
+
+    # Comments are related to article that commented at
+    article = models.ForeignKey(
+        Article, related_name="comments", on_delete=models.CASCADE
+    )
+
+    author = models.ForeignKey(User, related_name="comments", on_delete=models.CASCADE)
--- obsidian/users/models.py
+++ obsidian/users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
@@ -10,6 +11,24 @@ class User(AbstractUser):
     #: First and last name do not cover name patterns around the globe
     name = CharField(_("Name of User"), blank=True, max_length=255)
 
+    # This field will be empty when the user
+    # creates their account, so we specify `blank=True`.
+    bio = models.TextField(blank=True)
+
+    # Similar to `bio`, this field is not required. It may be blank.
+    image = models.URLField(blank=True)
+
+    # This is an example of a Many-To-Many relationship where both sides of the
+    # relationship are of the same model. In this case, the model is `user`.
+    # As mentioned in the text, this relationship will be one-way. Just because
+    # you are following mean does not mean that I am following you. This is
+    # what `symmetrical=False` does for us.
+    follows = models.ManyToManyField(
+        'self',
+        related_name='followed_by',
+        symmetrical=False
+    )
+
     def get_absolute_url(self):
         """Get url for user's detail view.
 
@@ -18,3 +37,20 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"username": self.username})
+
+    def follow(self, user):
+        """Follow `user` if we're not already following `user`."""
+        self.follows.add(user)
+
+    def unfollow(self, user):
+        """Unfollow `user` if we're already following `user`."""
+        self.follows.remove(user)
+
+    def is_following(self, user):
+        """Returns True if we're following `user`; False otherwise."""
+        return self.follows.filter(pk=user.pk).exists()
+
+    def is_followed_by(self, user):
+        """Returns True if `user` is following us; False otherwise."""
+        return self.followed_by.filter(pk=user.pk).exists()
+
