d5f01993cd16560d33a94495d63db22caa7fd189
jirenmaa <jirenmaa@gmail.com>
2020-09-22 17:14:05 +0700

    feat: detail article

--- obsidian/users/views.py
+++ obsidian/users/views.py
@@ -17,6 +17,17 @@ class UserDetailView(LoginRequiredMixin, DetailView):
     slug_field = "username"
     slug_url_kwarg = "username"
 
+    def get_context_data(self, **kwargs):
+        """Insert the single object into the context dict."""
+        context = {}
+        if self.object:
+            context['object'] = self.object
+            context['stories'] = Article.objects.filter(author=self.request.user.id)
+            context_object_name = self.get_context_object_name(self.object)
+            if context_object_name:
+                context[context_object_name] = self.object
+        context.update(kwargs)
+        return super().get_context_data(**context)
 
 user_detail_view = UserDetailView.as_view()
 
@@ -53,7 +64,7 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
 user_redirect_view = UserRedirectView.as_view()
 
 
-class UserStoryDetailView(LoginRequiredMixin, DetailView):
+class UserStoryDetailView(DetailView):
     model = Article
 
 
