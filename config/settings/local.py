c4e6a3bc954546625c7e68306bf43c18135cb434
jirenmaa <jirenmaa@gmail.com>
2020-09-28 17:08:34 +0700

    add allowed host

--- config/settings/local.py
+++ config/settings/local.py
@@ -13,7 +13,7 @@ SECRET_KEY = env(
     default=get_random_secret_key(),
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "aqueous-hollows-16248.herokuapp.com"]
 
 # CACHES
 # ------------------------------------------------------------------------------
--- config/settings/production.py
+++ config/settings/production.py
@@ -4,13 +4,18 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.integrations.logging import LoggingIntegration
 
+from django.core.management.utils import get_random_secret_key
+
 from .base import *  # noqa
 from .base import env
 
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = env("DJANGO_SECRET_KEY")
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default=get_random_secret_key()
+)
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
 
