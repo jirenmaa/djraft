9c2118e552b161063ae578050e008f679d6b1adb
jirenmaa <jirenmaa@gmail.com>
2020-10-01 18:05:06 +0700

    app(stories): update app.py

--- obsidian/stories/apps.py
+++ obsidian/stories/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class StoriesConfig(AppConfig):
-    name = 'stories'
+    name = 'obsidian.stories'
+    verbose_name = _("Stories")
