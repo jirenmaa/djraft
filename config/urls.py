694f87346f71d56b658894b988d6fdf8cb0c4c98
jirenmaa <jirenmaa@gmail.com>
2020-10-02 20:13:07 +0700

    home view

--- config/urls.py
+++ config/urls.py
@@ -7,8 +7,10 @@ from django.views import defaults as default_views
 from django.views.generic import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
 
+from .views import home
+
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path("", home, name="home"),
     path(
         "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
     ),
