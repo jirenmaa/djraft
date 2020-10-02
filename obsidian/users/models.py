f880968665ade520b295cb8f0d24a609f5ac3adf
jirenmaa <jirenmaa@gmail.com>
2020-10-02 18:06:16 +0700

    app(users): add new fields for user avatar in model

--- obsidian/users/models.py
+++ obsidian/users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -9,6 +10,9 @@ class User(AbstractUser):
 
     #: First and last name do not cover name patterns around the globe
     name = CharField(_("Name of User"), blank=True, max_length=255)
+
+    # This field is not required. It may be blank.
+    avatar = models.ImageField(blank=True)
 
     def get_absolute_url(self):
         """Get url for user's detail view.
