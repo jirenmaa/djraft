551cf3a6ec1e5a65f9b38518082c1d0620c2c9cf
jirenmaa <jirenmaa@gmail.com>
2020-10-02 18:03:29 +0700

    app(stories): use form widget instead of filter class in django template

--- obsidian/stories/forms.py
+++ obsidian/stories/forms.py
@@ -2,7 +2,7 @@
 import mimetypes
 
 from django import forms
-from stories.models import Story
+from .models import Story
 
 
 VALID_IMAGE_EXTENSION = [
@@ -47,3 +47,50 @@ class StoryForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(StoryForm, self).__init__(*args, **kwargs)
+        self.fields["title"].required = False
+        self.fields["description"].required = False
+        self.fields["cover"].required = False
+        self.fields["body"].required = False
+
+
+    title = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Title",
+                "required": False,
+                "autofocus": "off",
+                "autocomplete": "off",
+                "class": "appearance-none w-full h-full py-2 px-3 mb-4 text-gray-900 text-6xl leading-tight focus:outline-none",
+            }
+        ),
+    )
+
+    description = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "What is this about...",
+                "class": "appearance-none w-full py-2 px-3 mb-4 text-gray-700 text-xl leading-tight focus:outline-none",
+            }
+        )
+    )
+
+    cover = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                "placeholder": "Media image",
+                "class": "appearance-none py-2 px-3 mb-4 text-gray-700 leading-tight focus:outline-none",
+            }
+        )
+    )
+
+    body = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "Tell your story...",
+                "onkeyup": "AutoGrowTextArea(this)",
+                "style": "overflow:hidden",
+                "class": "appearance-none w-full py-2 px-3 mb-4 text-gray-700 text-xl leading-tight focus:outline-none",
+            }
+        )
+    )
