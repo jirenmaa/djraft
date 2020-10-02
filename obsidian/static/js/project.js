e46b8c88b6267b7388160eced86e778f4e553d65
jirenmaa <jirenmaa@gmail.com>
2020-10-02 18:02:36 +0700

    static(js): update project.js

--- obsidian/static/js/project.js
+++ obsidian/static/js/project.js
@@ -1,1 +1,11 @@
 /* Project specific Javascript goes here. */
+function profileDropdown() {
+  let el = document.getElementById("profile-setting-dropdown");
+  if (el.classList.contains("hidden")) {
+    el.classList.remove("hidden");
+    el.classList.add("active");
+  } else {
+    el.classList.remove("active");
+    el.classList.add("hidden");
+  }
+}
