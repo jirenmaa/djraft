45f60ecee8342fc848b5923041493b09ff0725d1
jirenmaa <jirenmaa@gmail.com>
2020-10-02 22:14:48 +0700

    prod

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,7 +14,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
-    default=get_random_secret_key()
+    default="83NkDFJAbFlIVOlMsgHxecMbHBNProQcjYgHLSpjiYYIYQrxYOsq5llE62icsDgRIVOlMsgHxec"
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["obsidians.herokuapp.com"])
