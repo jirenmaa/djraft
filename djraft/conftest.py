aac16b9892e3715f9b8395ad94eb2941899a1c8a
jirenmaa <jirenmaa@gmail.com>
2020-10-11 14:08:07 +0700

    prod(heroku): deploy#1

---
+++ djraft/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from djraft.users.models import User
+from djraft.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
