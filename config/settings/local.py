6e2da67ffe69c8483fa75e985b519c57bb79f5c4
jirenmaa <jirenmaa@gmail.com>
2020-11-16 11:01:49 +0700

    prod(heroku): deploy#4

--- config/settings/local.py
+++ config/settings/local.py
@@ -30,8 +30,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "django.djraft@gmail.com"
-EMAIL_HOST_PASSWORD = "apvqupaufhbdfcbm"
+EMAIL_HOST_USER = env("EMAIL_HOST")
+EMAIL_HOST_PASSWORD = env("PASS_HOST")
 # EMAIL_BACKEND = env(
 #     "DJANGO_EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend"
 # )
--- config/settings/production.py
+++ config/settings/production.py
@@ -137,8 +137,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env("DJANGO_ADMIN_URL")
-EMAIL_HOST_PASSWORD = env("DJANGO_ADMIN_URL")
+EMAIL_HOST_USER = env("EMAIL_HOST")
+EMAIL_HOST_PASSWORD = env("PASS_HOST")
 
 # django-compressor
 # ------------------------------------------------------------------------------
