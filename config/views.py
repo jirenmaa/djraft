fbbfaa76ece18335c06c3c4b2c1bf806b203c05c
jirenmaa <jirenmaa@gmail.com>
2020-11-16 17:12:16 +0700

    (prod, update): deploy#5, add some changes

--- config/views.py
+++ config/views.py
@@ -5,5 +5,5 @@ from stories.models import Story
 
 def home(request):
     if request.method == "GET":
-        articles = Story.objects.all()
+        articles = Story.objects.exclude(cover="")[:22]
         return render(request, "pages/home.html", context={"articles": articles})
