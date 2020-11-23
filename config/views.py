c36a2c15f1d9ebced2865c79e6a1d9540ada23c8
jirenmaa <jirenmaa@gmail.com>
2020-11-23 18:50:18 +0700

    fix: random query#3

--- config/views.py
+++ config/views.py
@@ -13,12 +13,12 @@ def home(request):
             cover__regex='.', description__isnull=False
         )
         # randomize explored articles
-        # list_explore_articles = []
-        # if web_explore_articles.exists() or web_main_articles.exists():
-        #     for _ in range(4):
-        #         list_explore_articles.append(Story.objects.random(web_explore_articles))
-        # else:
-        #     list_explore_articles = web_explore_articles
+        list_explore_articles = []
+        if web_explore_articles.exists() or web_main_articles.exists():
+            for _ in range(4):
+                list_explore_articles.append(Story.objects.random(web_explore_articles))
+        else:
+            list_explore_articles = web_explore_articles
 
         return render(
             request, "pages/home.html",
