cba7e139f68be3c3c6109639b408e8f9a4bf1774
jirenmaa <jirenmaa@gmail.com>
2020-11-23 15:21:05 +0700

    fix: key restrain slug (duplicated slug)

--- djraft/stories/utils.py
+++ djraft/stories/utils.py
@@ -3,7 +3,7 @@ from django.utils.text import slugify
 
 def generate_slug(title, created_at):
     title = title.replace(" ", "-").lower()
-    unique = (title[:7] + str(created_at)).encode("utf-8").hex()[-12:]
+    unique = (title[:7] + str(created_at)).encode("utf-8").hex()[49:65]
     slug = slugify("%s-%s" % (title, unique))
 
     return slug
