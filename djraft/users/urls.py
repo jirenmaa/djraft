1483bfa3321e09be2f2144e2dedc6383adb87155
jirenmaa <jirenmaa@gmail.com>
2020-12-11 19:15:13 +0700

    feat: Added user delete story action

--- djraft/users/urls.py
+++ djraft/users/urls.py
@@ -2,15 +2,15 @@ from django.urls import path
 
 from djraft.users.views import (
     user_detail_view,
-    user_redirect_view,
     user_update_view,
     user_story_detail_view,
+    user_story_delete,
 )
 
 app_name = "users"
 urlpatterns = [
     path("", view=user_detail_view, name="detail"),
     path("<slug:slug>", view=user_story_detail_view, name="article_detail"),
-    path("~redirect/", view=user_redirect_view, name="redirect"),
-    path("~update/", view=user_update_view, name="update"),
+    path("<slug:slug>/delete", view=user_story_delete, name="article_delete"),
+    path("~update", view=user_update_view, name="update"),
 ]
