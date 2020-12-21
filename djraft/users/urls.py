53de985535470d72800f282bad9466a0449c1932
jirenmaa <jirenmaa@gmail.com>
2020-12-21 19:56:49 +0700

    refactor: change update clause to settings

--- djraft/users/urls.py
+++ djraft/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from djraft.users.views import (
     user_detail_view,
-    user_update_view,
+    user_settings_view,
     user_story_detail_view,
     user_story_delete,
 )
@@ -12,5 +12,5 @@ urlpatterns = [
     path("", view=user_detail_view, name="detail"),
     path("<slug:slug>", view=user_story_detail_view, name="article_detail"),
     path("<slug:slug>/delete", view=user_story_delete, name="article_delete"),
-    path("~update", view=user_update_view, name="update"),
+    path("settings/", view=user_settings_view, name="settings"),
 ]
