babc983b70f59609e3e7a731e5b00df45e6bed7a
jirenmaa <jirenmaa@gmail.com>
2021-01-15 17:18:23 +0700

    update package.json

--- config/settings/production.py
+++ config/settings/production.py
@@ -116,7 +116,7 @@ COMPRESS_STORAGE = "compressor.storage.GzipCompressorFileStorage"
 # https://django-compressor.readthedocs.io/en/latest/settings/#django.conf.settings.COMPRESS_URL
 COMPRESS_URL = STATIC_URL  # noqa F405
 # https://django-compressor.readthedocs.io/en/latest/settings/#django.conf.settings.COMPRESS_OFFLINE
-COMPRESS_OFFLINE = False  # Offline compression is required when using Whitenoise
+COMPRESS_OFFLINE = True  # Offline compression is required when using Whitenoise
 # https://django-compressor.readthedocs.io/en/latest/settings/#django.conf.settings.COMPRESS_FILTERS
 COMPRESS_FILTERS = {
     "css": [
