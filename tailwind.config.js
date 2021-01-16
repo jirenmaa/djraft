cdd448d566de3e1b533446e1fa14f5d869e78329
jirenmaa <jirenmaa@gmail.com>
2021-01-16 18:28:32 +0700

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: ['./djraft/templates/**/*.html'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
